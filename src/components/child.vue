7bbd88466c6a7eaf9e4ebe17d30a9433c4afbe90
unknown <whoopsHzh@gmail.com>
2019-05-24 13:51:38 +0800

    基本传值

---
+++ src/components/child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3 @click='changeTitle()'>我是子元素</h3> 
+    <p>{{child}}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props:{
+      child:{
+        type :String,
+        Default :'我是默认的'
+      }
+  },
+  methods: {
+    changeTitle(){
+      this.$emit("titleChanged",'子组件向父组件传')
+    }
+  },
+}
+</script>
